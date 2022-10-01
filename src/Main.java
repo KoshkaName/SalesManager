72f9d66f46a27a66231c9680d6674d7133b31813
KoshkaName <114123386+KoshkaName@users.noreply.github.com>
2022-10-01 13:06:26 +1000

    stat

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         SalesManager salesManager = new SalesManager(new int[] {10, 20, 14, 9, 16, 7, 11});
 
         System.out.println("Максимальная продажа = " + salesManager.max());
+        System.out.println("Среднее обрезанное значение = " + salesManager.stat());
 
     }
 }
